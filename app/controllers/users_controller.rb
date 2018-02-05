c3bd64c0aa1ee665d78397859ed0e7905abfef43
hselle <harrison.selle@coloradocollege.edu>
2018-02-05 15:02:23 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
       #puts params[:login_password]
       session["song_names"] = ""
       session["record"] = ""
-      session["songs"] = ""
+      session["song_names"] = ""
 
       puts params
       @user = User.find_by_username(params[:login_username])
