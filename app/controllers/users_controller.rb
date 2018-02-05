5706304d2a64b5b7fc2551011f0911c5684e62e7
hselle <harrison.selle@coloradocollege.edu>
2018-02-05 16:15:11 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -143,7 +143,7 @@ class UsersController < ApplicationController
             
             @user.wins ||= ""
             @user.losses ||= ""
-            @user.songs ||= ""
+            #@user.songs ||= ""
             @user.records ||= ""
             @user.save!
             set_user_stats
